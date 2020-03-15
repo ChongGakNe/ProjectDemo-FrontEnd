f12b0383704709eae440e9a1d7c092ae945ca836
wh2per <93negaro@gmail.com>
2020-03-16 01:07:02 +0900

    replace fetch to axios

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,65 +2,64 @@ import React,{ useEffect, useState, useRef, useCallback} from 'react';
 import TodoTemplate from './TodoTemplate';
 import TodoInsert from './TodoInsert';
 import TodoList from './TodoList';
+import axios from 'axios';
 
 const Home = () =>{
     // const [todos, setTodos] = useState(createBulkTodos);
-     const [todos, setTodos] = useState([
-         {
-             id:0,
-             context:"aa",
-             checked:false
-         }
-     ]);
+    const [todos, setTodos] = useState([]);
 
-     useEffect(()=>{
-        console.log('aaaaab');
-        fetch('api/events')
-        .then(response => response.json())
-        .then(data=>setTodos(todos.concat(data)));
-     },[]);
+    const fetchUrl = async ()=> {
+        const response = await axios.get('api/events');
+        //const json = await response.json();
+        console.log(response);
+        setTodos(response.data);
+    }
+
+    useEffect(()=>{
+        fetchUrl();
+    },[]);
 
      // 고유값으로 사용될 id
      // ref를 사용하여 변수 담기
-     const nextId = useRef(4);
-   
-     const onInsert = useCallback(
-       text =>{
+    const nextId = useRef(4);
+
+    const onInsert = useCallback(
+        context =>{
          const todo = {
            id: nextId.current,
-           text,
+           context,
            checked:false
          };
-         setTodos(todos.concat(todo));
+         setTodos(todos => todos.concat(todo));
          nextId.current += 1;
+        },
+       [todos]
+    );
+   
+    const onRemove = useCallback(
+       id=>{
+         setTodos(todos => todos.filter(todo => todo.id !== id));
        },
        [todos]
-     );
+    );
    
-     const onRemove = useCallback(
-       id=>{
-         setTodos(todos.filter(todo => todo.id !== id));
-       },
-       [todos]
-     );
-   
-     const onToggle = useCallback(
-       id=>{
-         setTodos(
+    const onToggle = useCallback(
+        id=>{
+        setTodos( todos=>
            todos.map(todo=>
              todo.id === id ? { ...todo, checked: !todo.checked} : todo 
-           ),
+            ),
          );
-       },
-       [todos]
-     );
+        },
+        [todos]
+    );
    
-     return (
+    return (
      <TodoTemplate>
        <TodoInsert onInsert={onInsert} />
        <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
      </TodoTemplate>
-     );
-   };
+    );
+};
 
 export default Home;
